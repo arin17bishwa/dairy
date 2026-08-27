93e2c3a3897b1871d54605c50bceab58ab9fea05
bishwajit <arin17bishwa@gmail.com>
2026-08-27 16:34:05 +0530

    experiments

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import os
 
 import fastapi
 from dotenv import load_dotenv
+
+from src.context.SimpleContextBuilder import SimpleContextBuilder
 from src.embeddings.embedding_service import get_embeddings, get_embedding, similarity
-from src.ingestion.chunker import get_chunks
+from src.ingestion.chunker.JournalChunker import JournalChunker
 from src.ingestion.models import Journal, JournalEntry, Chunk, ChunkMetadata
 from src.ingestion.utils.chunker_utils import get_journal_data
+from src.llm.ollama import OllamaLLM
+from src.retrieval.SimpleRetrieverStore import SimpleRetriever
 from src.retrieval.faiss_store import FaissVectorStore
 from src.embeddings.SimpleEmbedding import SimpleEmbedding
+from src.retrieval.SimpleRetrieverStore import SimpleRetriever
 
 load_dotenv()
 
 def main():
-    query: str = "how horny am i?"
+    query: str = "give me a happy memory in 100 words"
     docs = get_journal_data().entries
 
     query_em = get_embedding(query)
@@ -38,16 +43,16 @@ def main():
 
 
 def faiss_init():
-    docs = get_journal_data().entries
-    docs_content=[doc.entry for doc in docs]
+    chunker=JournalChunker()
+    chunks=chunker.get_chunks(get_journal_data().entries)
 
     embedder=SimpleEmbedding(embedding_model_name=os.environ.get('EMBEDDING_MODEL_NAME'))
 
-    embeddings=embedder.get_embeddings(docs_content)
+    embeddings=embedder.get_embeddings([chunk.text for chunk in chunks])
 
     vs=FaissVectorStore(embedder.get_model().get_embedding_dimension())
 
-    vs.add(ids=[str(doc.date) for doc in docs], embeddings=embeddings)
+    vs.add(ids=[str(chunk.id) for chunk in chunks], embeddings=embeddings)
 
     vs.save(os.environ.get('VECTOR_DIR_FAISS'))
 
@@ -63,7 +68,7 @@ def faiss_main():
     embedder=SimpleEmbedding(embedding_model_name=os.environ.get('EMBEDDING_MODEL_NAME'))
     vs=FaissVectorStore.load(os.environ.get('VECTOR_DIR_FAISS'))
 
-    query: str = "how horny am i?"
+    query: str = "give me a happy memory in 100 words"
 
     q_embedding=embedder.get_embedding(query)
 
@@ -71,17 +76,47 @@ def faiss_main():
 
     print(matches)
 
+
 def ret_main():
+    chunker=JournalChunker()
 
     embedder=SimpleEmbedding(embedding_model_name=os.environ.get('EMBEDDING_MODEL_NAME'))
     vs=FaissVectorStore.load(os.environ.get('VECTOR_DIR_FAISS'))
-    chunks=get_chunks()
+    chunks=chunker.get_chunks(journal_entries=get_journal_data().entries)
+
+    data_store=SimpleRetriever(embedder=embedder, vector_store=vs, chunks=chunks)
+
+    query: str = "give me a happy memory in 100 words"
+
+    res=data_store.retrieve(query=query)
+
+def llm_00():
+
+    chunker=JournalChunker()
+
+    embedder=SimpleEmbedding(embedding_model_name=os.environ.get('EMBEDDING_MODEL_NAME'))
+    vs=FaissVectorStore.load(os.environ.get('VECTOR_DIR_FAISS'))
+    chunks=chunker.get_chunks(journal_entries=get_journal_data().entries)
+
+    data_store=SimpleRetriever(embedder=embedder, vector_store=vs, chunks=chunks)
+
+    query: str = "give me a happy memory in 100 words"
+
+    res=data_store.retrieve(query=query, k=5)
+    # print(res)
 
 
+    context_builder=SimpleContextBuilder()
+    user_prompt=context_builder.get_user_prompt(retrievals=res, query=query)
 
+    print(user_prompt)
 
+    llm=OllamaLLM(model='qwen3:4b')
+    response=llm.generate(user_prompt=user_prompt, system_prompt=context_builder.get_system_prompt())
+
+    print(response)
 
 
 
 if __name__ == "__main__":
-    faiss_main()
+    llm_00()
