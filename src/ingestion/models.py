8ae3a1262ccbc0102bb16c9ba2e9ce8b28140811
bishwajit <arin17bishwa@gmail.com>
2026-08-30 15:08:01 +0530

    updated domain model

--- src/ingestion/models.py
+++ src/ingestion/models.py
@@ -1,8 +1,9 @@
 import datetime
+from dataclasses import field
 from datetime import date
+import hashlib
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 
 class JournalEntry(BaseModel):
     entry: str
@@ -24,4 +25,33 @@ class Chunk(BaseModel):
     start_timestamp: datetime.datetime
     end_timestamp: datetime.datetime
     text: str
+    content_hash:str
     metadata: dict
+
+    @classmethod
+    def create(
+        cls,
+        *,
+        _id: str,
+        source: str,
+        start_timestamp: datetime.datetime,
+        end_timestamp: datetime.datetime,
+        text: str,
+        metadata: dict=None,
+    ) -> "Chunk":
+        if metadata is None:
+            metadata=dict()
+        return cls(
+            id=_id,
+            source=source,
+            start_timestamp=start_timestamp,
+            end_timestamp=end_timestamp,
+            text=text,
+            content_hash=hashlib.sha256(text.encode("utf-8")).hexdigest(),
+            metadata=metadata,
+        )
+
+
+    def update_text(self, text:str):
+        self.text=text
+        self.content_hash = hashlib.sha256(self.text.encode("utf-8")).hexdigest()
